c1989590def133dfa124824b3aca257787103b0f
TheM8thy <TheM8thy@proton.me>
2024-05-21 12:37:26 +0200

    Update lp_finder.py

--- lp_finder.py
+++ lp_finder.py
@@ -1,6 +1,6 @@
 import argparse
 import requests
-from bs4 import BeautifulSoup
+import sqlite3
 #from geopy import distance
 
 def formatter(prog):
@@ -9,7 +9,7 @@ def formatter(prog):
 
     return argparse.HelpFormatter(prog, max_help_position=64)
 
-def parseargs() -> dict:
+def parseargs():
     r"""Parses the args and returns them as a dictionary
     """
 
@@ -44,13 +44,6 @@ def distance(start_coords : tuple, end_coords : tuple) -> int:
     api: requests.Response = requests.get(url=f"https://api.openrouteservice.org/v2/directions/driving-car?api_key=5b3ce3597851110001cf6248c7259f9c823e47aaa576f83dea8c410d&start={start_coords[1]},{start_coords[0]}&end={end_coords[1]},{end_coords[0]}", timeout=10)
     print(api.json()["features"][0]["properties"]["summary"]["distance"])
 
-def lostplace_club_soupify(html : str):
-    r"""Parses a given html document into a list of lost place dictionaries
-    
-    :param html: The html to be parsed
-    """
-    return BeautifulSoup(markup=html, features='html.parser')
-
 def get_coordinates(city : str) -> tuple:
     r"""Retrieves the coordinates of a given city and returns them in a tuple
 
@@ -62,79 +55,12 @@ def get_coordinates(city : str) -> tuple:
     
     return (nominatim_r.json()[0].get("lat"),nominatim_r.json()[0].get("lon"))
 
-def get_lps_lostplace_club(coords : tuple, city_name : str, cached_html_eng=False, cached_html=False, debug = False) -> list:
-    r"""Retrieves lp's around given coordinates
-
-    :param coords: The coordinates of the location around which lp's are to be searched
-    """
-    
-    if not cached_html_eng:
-        lostplace_club_eng_url: str = f"https://lostplace.club/listview.php?lat={coords[0]}&lon={coords[1]}"
-        lostplace_club_eng_r: requests.Response = requests.get(url=lostplace_club_eng_url, timeout=10)
-        soup_base_eng = lostplace_club_soupify(html=lostplace_club_eng_r.text)
-        lps_html_eng = soup_base_eng.css.select("body > div.container > div.listview2__container > div.listview2__item")
-        i=0
-        while city_name.lower() in lps_html_eng[-1].css.select_one(".bi-geo").parent.span.text.split(":")[1].lower():
-            lostplace_club_eng_url: str = f"https://lostplace.club/listview.php?lat={coords[0]}&lon={coords[1]}&page={i}"
-            lostplace_club_eng_r: requests.Response = requests.get(url=lostplace_club_eng_url, timeout=10)
-            soup_base_eng = lostplace_club_soupify(html=lostplace_club_eng_r.text)
-            lps_html_eng += soup_base_eng.css.select("body > div.container > div.listview2__container > div.listview2__item")
-            i+=1
-    else:
-        soup_base_eng = lostplace_club_soupify(html=cached_html_eng)
-        lps_html_eng = soup_base_eng.css.select("body > div.container > div.listview2__container > div.listview2__item")
-    
-    lps: list = {}
-    for lp_html in lps_html_eng:
-            if city_name.lower() in lps_html_eng[-1].css.select_one(".bi-geo").parent.span.text.split(":")[1].lower() or debug:
-                lp: dict = {}
-                id: str = lp_html.css.select_one("div.listview2__item-headline").text.split(" (#")[1].split(")")[0].strip()
-                lp.update({"name":lp_html.css.select_one("div.listview2__item-headline").text.split(" (#")[0].strip()})
-                lp.update({"coords":(lp_html.css.select_one("i.bi-arrow-bar-right").parent.span.text.split(":")[1].strip(),
-                lp_html.css.select_one("i.bi-arrow-bar-up").parent.span.text.split(":")[1].strip())})
-                lp.update({"pic_url":f'https://lostplace.club{lp_html.css.select_one("div.listview2__item-picture").get("style").split("url(")[1][2:].split(")")[0][:-1].strip()}'})
-                lp.update({"comment_cnt": lp_html.css.select_one(".bi-pencil").parent.span.text.split(":")[1].strip()})
-                #lp.update({"dist": lp_html.css.select_one(".bi-cursor").parent.span.text.split(":")[1].strip().split("Kilo")[0].strip()})
-                #lp.update({"dist": distance(start_coords=coords,end_coords=lp["coords"])})
-                lps.update({id:lp})
-
-    if not cached_html:
-        lostplace_club_url: str = f"https://lostplace.club/listenansicht.php?lat={coords[0]}&lon={coords[1]}"
-        lostplace_club_r: requests.Response = requests.get(url=lostplace_club_url, timeout=10)
-        soup_base = lostplace_club_soupify(html=lostplace_club_r.text)
-    else:
-        soup_base = lostplace_club_soupify(html=cached_html)
-
-    
-    lps_html = soup_base.css.select("body > div > a[href^='./place.php?id=']")
-    for lp_html in lps_html:
-        id = lp_html.css.select_one(".bi-hash").parent.text.strip()
-        if id in lps.keys():
-            lps[id].update({"pic_cnt": lp_html.css.select_one(".bi-images").parent.text.strip()})
-        #else:
-        #    lps[id] = {"Bilder": lp_html.css.select_one(".bi-images").parent.text.strip()}
-        #lps.append({"name":lp_html.css.select_one(".listenansicht__top-information-heading").text.strip(),
-        #            "id":lp_html.css.select_one(".bi-hash").parent.text.strip(),
-        #            "lat":lp_html.css.select_one(".bi-geo-alt").parent.text.split(" - ")[0].strip(),
-        #            "lon":lp_html.css.select_one(".bi-geo-alt").parent.text.split(" - ")[1].strip(),
-        #            "pic_url":f'https://lostplace.club{lp_html.css.select_one(".listenansicht__image").get("data-bg").strip()}'})
-
-    return lps
-
 def main() -> None:
     r"""Main function
     """
-    args: dict = parseargs()
+
+    args = parseargs()
     city_coords: tuple = get_coordinates(city=args["city"][0])
-    if args["debug"]:
-        with open(file="lost_place_club_eng.txt",mode="r",encoding="utf-8") as fp:
-            soup_eng: str = fp.read()
-        with open(file="lost_place_club.txt",mode="r",encoding="utf-8") as fp:
-            soup: str = fp.read()
-        lps: list = get_lps_lostplace_club(coords=city_coords, city_name=args["city"][0], cached_html_eng=soup_eng, cached_html=soup, debug=True)
-    else:
-        lps: list = get_lps_lostplace_club(coords=city_coords, city_name=args["city"][0])
-    print(len(lps))
 
 if __name__ == "__main__":
     main()
@@ -142,6 +68,6 @@ if __name__ == "__main__":
 
 # []
 # {}
-
+# \
 
 # filter stadtname, radius
